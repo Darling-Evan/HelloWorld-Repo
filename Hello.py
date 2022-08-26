3abc87b6aef0b770bba3c8b074752634c9353c42
Darling-Evan <112196089+Darling-Evan@users.noreply.github.com>
2022-08-26 12:02:29 -0500

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,45 @@
+# My First Python Program
+
+space = " " 
+x = "Bruh"
+y = "Hello World"
+z = "My name is Evan"
+
+print(y)
+print(space)
+print("Wassup")
+print(space)
+print(z)
+print(space)
+print(x)
+print(space)
+print(200+40)
+print(400/23)
+print(248*298)
+
+print(space)
+
+a = 24
+b = 245
+if b > a:
+	print("b is greater than a")
+
+print(space)
+
+c = 44
+d = 44
+if c > d:
+	print("c is greater than d")
+elif c == d:
+	print("c and d are equal")
+
+print(space)
+
+e = 200
+f = 33
+if e > f:
+	print("e is greater than f")
+elif e == f:
+	print("e and f are equal")
+else:
+	print("e is greater than f")
